2fde70fcd0bcc227eab73c36d9d147a80b7c6d0c
Nathaniel <everalert@users.noreply.github.com>
2025-12-18 19:21:23 +1100

    chore: round out branchless utils

--- src/util.zig
+++ src/util.zig
@@ -1,28 +1,36 @@
 const std = @import("std");
 
-// TODO: confirm these actually make a difference vs natural codegen
-
-/// branchless and -> integer
+/// Branchless AND -> int
 pub inline fn IBLAND(b1: bool, b2: bool) usize {
     return @intFromBool(b1) & @intFromBool(b2);
 }
 
-/// branchless and
+/// Branchless AND
 pub inline fn BLAND(b1: bool, b2: bool) bool {
     return IBLAND(b1, b2) > 0;
 }
 
-/// branchless or -> integer
+/// Branchless OR -> int
 pub inline fn IBLOR(b1: bool, b2: bool) usize {
     return @intFromBool(b1) | @intFromBool(b2);
 }
 
-/// branchless or
+/// Branchless OR
 pub inline fn BLOR(b1: bool, b2: bool) bool {
     return IBLOR(b1, b2) > 0;
 }
 
-/// add spaces up to given column, adding a minimum of 1 space for padding
+/// Branchless XOR -> int
+pub inline fn IBLXOR(b1: bool, b2: bool) usize {
+    return @intFromBool(b1) ^ @intFromBool(b2);
+}
+
+/// Branchless XOR
+pub inline fn BLXOR(b1: bool, b2: bool) bool {
+    return IBLXOR(b1, b2) > 0;
+}
+
+/// Add spaces up to given column, adding a minimum of 1 space for padding
 pub fn PadSpaces(out: *std.ArrayListUnmanaged(u8), col: *usize, until: usize) void {
     const n: usize = @max(1, until -| col.*);
     out.appendNTimesAssumeCapacity(32, n);
